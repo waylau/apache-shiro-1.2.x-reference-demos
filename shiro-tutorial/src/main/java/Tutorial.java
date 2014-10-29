b8dd481a27de6485b5962c9a55a807816342a603
waylau <waylau521@gmail.com>
2014-10-29 14:39:35 +0800

    add  "shiro-tutorial"

---
+++ shiro-tutorial/src/main/java/Tutorial.java
@@ -0,0 +1,19 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tutorial {
+
+    private static final transient Logger log = LoggerFactory.getLogger(Tutorial.class);
+
+    public static void main(String[] args) {
+        log.info("My First Apache Shiro Application");
+        System.exit(0);
+    }
+}
